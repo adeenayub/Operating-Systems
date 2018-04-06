aa6736718b4988676fd055141312e469df25ddff
adeenayub <12beseaayub@seecs.edu.pk>
2018-04-06 09:15:21 +0500

    Update client.c

--- Assignment 3/client.c
+++ Assignment 3/client.c
@@ -1,4 +1,4 @@
-//Bugs removed Alhamdulillah but final touches to be given
+//Control C working as well Alhamdulillah. This may be the final version IA. 
 #include <stdio.h>
 #include <sys/socket.h>
 #include <stdlib.h>
@@ -9,20 +9,7 @@
 #define PORT 8080
 #define BUFFSIZE 1024
 
-int skt = 0;
-struct sockaddr_in serv_address; 
-#include <stdio.h>
-#include <sys/socket.h>
-#include <stdlib.h>
-#include <string.h>
-#include <netinet/in.h>
-#include <sys/socket.h>
 
-#define PORT 8080
-#define BUFFSIZE 1024
-
-int skt = 0;
-struct sockaddr_in serv_address; 
 
 void main()
 {
@@ -32,6 +19,8 @@ void main()
 	char *str;
 	int len;
 	printf("Assalamualaikum! I am the client. Nice to see you!");
+	int skt = 0;
+	struct sockaddr_in serv_address; 
 	skt = socket(AF_INET,SOCK_STREAM ,0);
 	if(skt < 0)
 	{
@@ -63,46 +52,5 @@ void main()
 			send(skt,line,strlen(line),0 );
 		}	
 	}
-	
-}
-void main()
-{
-	char *line = malloc(sizeof(char)*BUFFSIZE); 
-	char *buff = malloc(sizeof(char)*BUFFSIZE);
-	char substr[]= "C00L";
-	char *str;
-	int len;
-	printf("Assalamualaikum! I am the client. Nice to see you!");
-	skt = socket(AF_INET,SOCK_STREAM ,0);
-	if(skt < 0)
-	{
-		perror("Socket not created\n");
-		exit(1);
-	}
-	memset(&serv_address, '0', sizeof(serv_address));
-	serv_address.sin_family = AF_INET;
-    	serv_address.sin_port = htons( PORT );
-
-	if(inet_pton(AF_INET, "127.0.0.1", &serv_address.sin_addr)<=0) 
-    	{
-		perror("The address is not valid \n");
-		exit(1);
-    	}
-	if (connect(skt, (struct sockaddr *)&serv_address, sizeof(serv_address)) < 0)
-    	{
-        	printf("The client has failed to connect to server \n");
-        	exit(1);
-    	}
-	//while(1)
-	//{
-		printf("Enter a string containing alphabets and/or numbers");
-		fgets(line, BUFFSIZE, stdin);
-		printf("%s", line);
-		len = strlen(line);
-		if(strstr(line,substr))
-		{
-			send(skt,line,strlen(line),0 );
-		}	
-	//}
 	
 }
